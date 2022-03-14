d682e6af4a831b8ee8247ad03c8f5d0c2cca538f
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-03-14 16:27:45 +1100

    Created script.js

---
+++ script.js
@@ -0,0 +1,99 @@
+/ Feature #1-displaying current time and date
+function formatTime(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayList = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayList];
+  return `${day} ${hours}:${minutes}`;
+}
+
+let dateElement = document.querySelector("#currentDate");
+let currentTime = new Date();
+
+dateElement.innerHTML = formatTime(currentTime);
+
+// Feature #2- add a search engine, and display name of search on webpage in lieu of Budapest+HW 5- //Homework 5- put in actual weather of city that user searches. also add current locatio button that gets GPS coordinates+city name with curent ltemp
+
+function showLocation(city) {
+  let apiKey = "7bcf0da6ca80b20c501d86d32cc003a7";
+  let units = "metric";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiURL).then(showTemp);
+}
+
+	function showTemp(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#temperature").innerHTML = Math.round(
+      response.data.main.temp
+    );
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind-speed").innerHTML = Math.round(
+      response.data.wind.speed
+    );
+    document.querySelector("#description").innerHTML =
+      response.data.weather[0].main;
+  }
+
+function search(event) {
+  event.preventDefault();
+
+  let cityInput = document.querySelector("#city-input");
+  showLocation(cityInput.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+// Bonus Feature<--taken from solution-converting C to F
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+//Bonus- add current Location feature
+
+//currentLocation.innerHTML = showLocation.position;
+function getWeatherLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "7bcf0da6ca80b20c501d86d32cc003a7";
+  let units = "metric";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+  axios.get(apiURL).then(showTemp);
+}
+function getPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getWeatherLocation);
+}
+
+let currentLocation = document.querySelector("#current-location");
+currentLocation.addEventListener("click", getPosition);
